2c78a6f757221f01e43e16388fb6aa9c55929e04
Jeff Swearingen <jeffdevelopmentacct@gmail.com>
2017-02-08 16:50:51 -0500

    heroku fix a

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,27 @@
-
-var mysql = require('mysql');
-
+var Sequelize = require('sequelize');
+var sequelize;
+//var mysql = require('mysql');
+var env;
+if (process.env.JAWSDB_URL)
+    env = "production";
+else {
+    env = 'development';
+}
 var connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+var config = require('./config')[env];
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burgers_db"
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: config.dialect
     });
+    //     host: "127.0.0.1",
+    //     user: "root",
+    //     password: "root",
+    //     database: "burgers_db"
+    // });
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
 
-module.exports = connection;
+module.exports = sequelize;
--- models/burger.js
+++ models/burger.js
@@ -3,29 +3,34 @@ var sequelize = require('../config/connection.js');
 
 //create a model of the table for sequelize
 var Burgers = sequelize.define('burgers', {
-        burger_name: {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    burger_name: {
             type: Sequelize.STRING,
             unique: true,
             allowNull: false,
             validate: {
                 len: {
                     args: [6, 25],
-                    msg: 'Please enter a title wtih at least 6 chars but no more than 15'
+                    msg: 'Please enter a title with at least 6 chars but no more than 15'
                 }
             }
-        },
-        devoured: {
+    },
+    devoured: {
             type: Sequelize.BOOLEAN,
             defaultValue: 0,
-        },
-        date: {
+    },
+    date: {
             type: Sequelize.DATE,
             defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-        }
-    },
-    {
+    }
+},
+{
         timestamps: false
-    });
+});
 
 
 module.exports = Burgers;
