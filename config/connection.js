f7816271c64c917e0ef9344efcdcf3dd38009a07
Jeff Swearingen <jeffdevelopmentacct@gmail.com>
2017-02-08 14:03:05 -0500

    heroku modifications3

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,49 @@
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
+//
+// var env;
+//
+// if (process.env.JAWSDB_URL)
+//     env = "production";
+// else {
+//     env = "development";
+// }
+//
+// var config = require('./config')[env];
+//
+// var sequelize;
+//
+// if (config.use_env_variable) {
+//     sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//     sequelize = new Sequelize(config.database, config.username, config.password, {
+//         host: config.host,
+//         dialect: config.dialect
+//     });
+// }
+//
+// module.exports = sequelize;
 
-var env;
+var mysql = require('mysql');
 
-if (process.env.JAWSDB_URL)
-    env = "production";
-else {
-    env = "development";
-}
+var connection;
 
-var config = require('./config')[env];
-
-var sequelize;
-
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, {
-        host: config.host,
-        dialect: config.dialect
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
     });
-}
+};
 
-module.exports = sequelize;
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
